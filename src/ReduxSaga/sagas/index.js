7d05bd32c6992d4ab2ce9232b0d5a0668b75430a
Solleti Venkata Sai Lakshmana Gupta <solletivenkata.l@tataelxsi.co.in>
2022-12-06 17:38:03 +0530

    Phase-2

---
+++ src/ReduxSaga/sagas/index.js
@@ -0,0 +1,10 @@
+import { spawn } from 'redux-saga/effects'
+
+// Sagas 
+import todoSaga from './todo-saga'
+
+// Export the root saga 
+export default function* rootSaga() {
+    // console.log("Hello From Redux-Saga!")
+    yield spawn(todoSaga)
+} 
